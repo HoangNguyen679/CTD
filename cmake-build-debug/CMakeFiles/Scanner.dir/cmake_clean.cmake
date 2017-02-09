7d860e4b1a44e225d9afb459978382e18c44ff66
Hoang Nguyen <ndhoang96@gmail.com>
2017-02-09 21:15:09 +0700

    init CTD - scanner

---
+++ cmake-build-debug/CMakeFiles/Scanner.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scanner.dir/charcode.c.o"
+  "CMakeFiles/Scanner.dir/error.c.o"
+  "CMakeFiles/Scanner.dir/reader.c.o"
+  "CMakeFiles/Scanner.dir/scanner.c.o"
+  "CMakeFiles/Scanner.dir/token.c.o"
+  "Scanner.pdb"
+  "Scanner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Scanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
